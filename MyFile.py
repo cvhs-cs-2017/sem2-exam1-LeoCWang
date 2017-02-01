9431890ea0a821650956bfde6c2422f24a5897f3
LeoCWang <leowang03@hotmail.com>
2017-02-01 10:28:20 -0800

    DrawRectangle and DrawPoly done

--- MyFile.py
+++ MyFile.py
@@ -2,3 +2,25 @@ def AddTen(n):
     n = n + 10
     return (n)
 print (AddTen(5))
+
+
+import turtle
+Tim = turtle.Turtle()
+def DrawRectangle(Anyturtle, l, w):
+    for i in range(2):
+        Anyturtle.forward(l)
+        Anyturtle.left(90)
+        Anyturtle.forward(w)
+        Anyturtle.left(90)
+    input()
+    Anyturtle.clear()
+DrawRectangle(Tim,100,50)
+
+
+def DrawPoly(Anyturtle, n):
+    for i in range(n):
+        Anyturtle.forward(50)
+        Anyturtle.left(360/n)
+    input()
+    Anyturtle.clear()
+DrawPoly(Tim,15)
