46684928a75937249d60e601ec3ca8c59fe759a1
LeoCWang <leowang03@hotmail.com>
2017-02-01 10:33:33 -0800

    Function  done

--- Function.py
+++ Function.py
@@ -6,8 +6,11 @@ def triple(n):
     return (n)
 print(triple(5))
 
-
-
 """Write a program that will prompt the user for an input value (n) and print
 the result of 3n by calling the function defined above.  Make sure you include
 the necessary print statements and address any issues with whitespace. """
+
+def usertriple(n):
+    n = 3 *n
+    return n
+print (usertriple(int(input('Please enter a number'))))
