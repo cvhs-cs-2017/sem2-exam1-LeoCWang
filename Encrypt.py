b3a7565f8d342dd59fa8408d781c1153931f3ade
LeoCWang <leowang03@hotmail.com>
2017-02-01 10:15:31 -0800

    problem 1 done

--- Encrypt.py
+++ Encrypt.py
@@ -3,11 +3,18 @@
 'Computer Science Makes the World go round but it doesn't make the world round itself!'
 
 Print the save the result as the variable = NoVowels
-""""
+"""
 
+plainText = 'Computer Science Makes the World go round but it doesn\'t make the world round itself!'
 
-
-
-
+def encrypt(plainText):
+    NoVowels = ""
+    for ch in plainText:
+        if ch == 'a' or ch == 'i' or ch == 'e' or ch == 'o' or ch == 'u' or ch == 'y':
+            NoVowels = NoVowels + ''
+        else:
+            NoVowels = NoVowels + ch
+    return(NoVowels)
+print (encrypt(plainText))
 
 """Write an encryption code that you make up and run it for the variable NoVowels"""
